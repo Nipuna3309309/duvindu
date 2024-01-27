21189e8dbaca6686cf368785d16320385896a925
nipuna <IT22154576@my.sliit.lk>
2024-01-27 12:44:14 +0530

    main
    sdsd

---
+++ SLIIT/e-shop/hooks/useCart.tsx
@@ -0,0 +1,147 @@
+import { CartProductType } from "@/app/product/ProductDetails";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
+import { toast } from "react-hot-toast"
+
+type cartContextType ={
+   cartTotalQty: number  
+   cartProducts: CartProductType[] | null
+   handleAddProductToCart: (product: CartProductType) => void
+   handleRemoveProductFromCart: (product: CartProductType) => void
+   handleCartQtyIncrease: (product: CartProductType) => void
+   handleCartQtyDecrease: (product: CartProductType) => void
+   handleClearCart: () => void
+   
+
+}
+
+export const CartContext = createContext<cartContextType | null>(null)
+
+interface Props{
+    [propName: string]: any
+}
+
+export const CartContextProvider = (props: Props) =>{
+    const [cartTotalQty, setCartTotalQty] = useState(10)
+
+    const [cartProducts, setCartProducts] = useState<CartProductType[] | null>(null)
+
+    useEffect(() => {
+        const cartItems: any =localStorage.getItem('DSR')
+        const cProducts: CartProductType[] | null = JSON.parse(cartItems)
+
+        setCartProducts(cProducts)
+    }, [])
+
+    const handleAddProductToCart = useCallback((product: CartProductType) => {
+        setCartProducts((prev) =>{
+            let updatedCart;
+
+            if(prev){
+                updatedCart = [...prev, product]
+            }
+            else{
+                updatedCart = [product]
+            }
+          //const notificationId = product.id || Date.now();       
+        
+        toast.success("Product added to Cart")
+
+        localStorage.setItem('DSR', JSON.stringify(updatedCart))
+
+        return updatedCart
+        })
+    }, []);
+
+    const handleRemoveProductFromCart = useCallback((
+        product: CartProductType 
+    ) => {
+        if(cartProducts){
+            const filteredProducts = cartProducts.filter((item) => {
+                return item.id !== product.id
+            })
+
+            setCartProducts(filteredProducts)
+            toast.success("Product Removed")
+            localStorage.setItem('DSR', JSON.stringify(filteredProducts))
+
+        }
+    }, [cartProducts])
+
+    const handleCartQtyIncrease = useCallback((
+        product: CartProductType 
+    ) => {
+        let updatedCart;
+
+        if(product.quantity === 10){
+            return toast.error("Maximum reached!")
+        }
+
+        if(cartProducts){
+            updatedCart = [...cartProducts]
+
+            const existingIndex = cartProducts.findIndex((item) => item.id ===product.id)
+        
+            if(existingIndex > -1){
+                updatedCart[existingIndex].quantity = ++updatedCart[existingIndex].quantity
+            }
+
+            setCartProducts(updatedCart)
+            localStorage.setItem('DSR', JSON.stringify(updatedCart))
+        }
+
+    }, [cartProducts] )
+
+    const handleCartQtyDecrease = useCallback((
+        product: CartProductType 
+    ) => {
+        let updatedCart;
+
+        if(product.quantity === 1){
+            return toast.error("Oops! Minumum reached!")
+        }
+
+        if(cartProducts){
+            updatedCart = [...cartProducts]
+
+            const existingIndex = cartProducts.findIndex((item) => item.id ===product.id)
+        
+            if(existingIndex > -1){
+                updatedCart[existingIndex].quantity = --updatedCart[existingIndex].quantity
+            }
+
+            setCartProducts(updatedCart)
+            localStorage.setItem('DSR', JSON.stringify(updatedCart))
+        }
+
+    }, [cartProducts] )
+
+    const handleClearCart = useCallback(() => {
+        setCartProducts(null)
+        setCartTotalQty(0)
+
+        localStorage.setItem('DSR', JSON.stringify(null))
+
+    }, [cartProducts])
+
+    const value = {
+        cartTotalQty,
+        cartProducts,
+        handleAddProductToCart,
+        handleRemoveProductFromCart,
+        handleCartQtyIncrease,
+        handleCartQtyDecrease,
+        handleClearCart
+    }
+
+    return <CartContext.Provider value={value} {...props} />
+}
+
+export const useCart =() =>{
+    const context = useContext(CartContext)
+
+    if(context === null){
+        throw new Error("useCart must be used within a CartContextProvider")
+    }
+
+    return context
+}
